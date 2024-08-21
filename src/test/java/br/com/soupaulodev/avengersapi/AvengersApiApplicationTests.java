f1c7b2bff4e14d44535bbabd257effeec2083195
Paulo Marques <paulomarques.rs1@gmail.com>
2024-08-20 23:29:03 -0300

    Init project

---
+++ src/test/java/br/com/soupaulodev/avengersapi/AvengersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.soupaulodev.avengersapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvengersApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
